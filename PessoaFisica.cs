cd57e90a520f826d7840667cd0e27ce2caf1639d
Eduardo Vilar <e.vilar95@gmail.com>
2022-01-27 14:35:50 -0300

    Encontro Remoto 2

---
+++ PessoaFisica.cs
@@ -0,0 +1,13 @@
+namespace EncontroRemoto
+{
+    public class PessoaFisica : Pessoa
+    {
+        public string? cpf { get; set; }
+
+        public DateTime dataNascimento { get; set; }
+        
+        
+        
+        
+    }
+}
---
+++ PessoaJuridica.cs
@@ -0,0 +1,11 @@
+namespace EncontroRemoto
+{
+    public class PessoaJuridica : Pessoa
+    {
+        public string? cnpj { get; set; }
+        
+        public string? razaoSocial { get; set; }
+        
+        
+    }
+}
